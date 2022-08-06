e6e87537e62feb0488346e0da45d13cfd0dcd4a8
darshkaushik <darsh.kaushik@gmail.com>
2022-08-06 12:19:08 +0530

    Update reconstruct.py

--- reconstruct.py
+++ reconstruct.py
@@ -44,7 +44,8 @@ def parse_args():
 						type=str)
     parser.add_argument('--ground',
 						help='whether to construct ground truth sequences',
-						type=int)
+						default=0,
+                        type=int)
 
     args, _ = parser.parse_known_args()
     with open(args.cfg, 'r') as stream:
@@ -76,16 +77,21 @@ def main():
     args = parse_args()
 
     # seq_names = ["00017","00018","00002","00014","00005","00010"]
-    seq_names = ['01699', #'02855', 
-       '00826', '02031', '01920', '02664', '01834',
-       '02859', '00398', '03886', '01302', '02053', '00898', '03592',
-       '03580', '00771', '01498', '00462', '01292', '02441', '03393',
-       '00376', '02149', '03200', '03052', '01788', '00514', '01744',
-       '02977', '00243', '02874', '00396', '03597', '02654', '03703',
-       '00456', '00812', '00979', '00724', '01443', '03401', '00548',
-       '00905', '00835', #'02612', 
-       '02388', '03788', '03870', '03181',
-       '00199']
+    # seq_names = ['01699', #'02855', 
+    #    '00826', '02031', '01920', '02664', '01834',
+    #    '02859', '00398', '03886', '01302', '02053', '00898', '03592',
+    #    '03580', '00771', '01498', '00462', '01292', '02441', '03393',
+    #    '00376', '02149', '03200', '03052', '01788', '00514', '01744',
+    #    '02977', '00243', '02874', '00396', '03597', '02654', '03703',
+    #    '00456', '00812', '00979', '00724', '01443', '03401', '00548',
+    #    '00905', '00835', #'02612', 
+    #    '02388', '03788', '03870', '03181',
+    #    '00199']
+    seq_names = [
+        "01459","00803","00656","03089","03098","00066","00943","01715","02349",
+        "02521","01930","03740","03375","02356","00643","03959","00026","00587",
+        "03017","02765","01409","01310","02320","02188","03898","03102","01156",
+        "01784","00057","01175"]
     
     if args["PRETRAIN"]["DATA"]["DATA_SPLITS"] != None:
         with open(os.path.join(args["PRETRAIN"]["DATA"]["DATA_DIR"], args["PRETRAIN"]["DATA"]["DATA_NAME"] + '_data_split.json'), 'r') as handle:
@@ -106,8 +112,8 @@ def main():
         split='val'
     frame2cluster_mapping_path = os.path.join(log_dir, f'advanced_{split}_res_{args["K"]}.pkl')
     contiguous_frame2cluster_mapping_path = os.path.join(log_dir, f'advanced_{split}_{args["K"]}.pkl')
-    cluster2keypoint_mapping_path = os.path.join(log_dir, f'proxy_centers_{split}_{args["K"]}.pkl')
-    cluster2frame_mapping_path = os.path.join(log_dir, f'proxy_centers_{split}_complete_{args["K"]}.pkl')
+    cluster2keypoint_mapping_path = os.path.join(log_dir, f'proxy_centers_tr_{args["K"]}.pkl')
+    cluster2frame_mapping_path = os.path.join(log_dir, f'proxy_centers_tr_complete_{args["K"]}.pkl')
 
     
     if args["FRAMES_DIR"] == None:
@@ -128,9 +134,9 @@ def main():
 
     else:
         #No filter
-        very_naive_mpjpe_mean = very_naive_reconstruction(seq_names, data_path, frame2cluster_mapping_path, cluster2keypoint_mapping_path, args["SK_TYPE"], frames_dir=args["FRAMES_DIR"]+'very_naive')
-        naive_mpjpe_mean = naive_reconstruction(seq_names, data_path, contiguous_frame2cluster_mapping_path, cluster2frame_mapping_path, args["SK_TYPE"], frames_dir=args["FRAMES_DIR"]+'naive')
-        naive_no_rep_mpjpe_mean, faulty = naive_reconstruction_no_rep(seq_names, data_path, contiguous_frame2cluster_mapping_path, cluster2frame_mapping_path, args["SK_TYPE"], frames_dir=args["FRAMES_DIR"]+'naive_no_rep')
+        # very_naive_mpjpe_mean = very_naive_reconstruction(seq_names, data_path, frame2cluster_mapping_path, cluster2keypoint_mapping_path, args["SK_TYPE"], frames_dir=args["FRAMES_DIR"]+'very_naive')
+        # naive_mpjpe_mean = naive_reconstruction(seq_names, data_path, contiguous_frame2cluster_mapping_path, cluster2frame_mapping_path, args["SK_TYPE"], frames_dir=args["FRAMES_DIR"]+'naive')
+        # naive_no_rep_mpjpe_mean, faulty = naive_reconstruction_no_rep(seq_names, data_path, contiguous_frame2cluster_mapping_path, cluster2frame_mapping_path, args["SK_TYPE"], frames_dir=args["FRAMES_DIR"]+'naive_no_rep')
         
         #uniform filter
         uni_very_naive_mpjpe_mean = very_naive_reconstruction(seq_names, data_path, frame2cluster_mapping_path, cluster2keypoint_mapping_path, args["SK_TYPE"], filter = 'uniform', frames_dir=args["FRAMES_DIR"]+'very_naive_ufilter')
@@ -144,7 +150,7 @@ def main():
     
     if args["GROUND"]:
         #original video
-        ground_truth_construction(seq_names, data_path, args["SK_TYPE"], frames_dir=args["FRAMES_DIR"]+'ground')
+        ground_truth_construction(seq_names, data_path, args["SK_TYPE"], frames_dir='./kit_reconstruction/ground/')
 
     print('very naive mpjpe : ', very_naive_mpjpe_mean)
     print('naive mpjpe : ', naive_mpjpe_mean)
