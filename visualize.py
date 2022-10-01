3f8bb2f268ba99aca071a04a6b71c979fc9802c1
Arjun Chandrasekaran <arjun.chandrasekaran@tuebingen.mpg.de>
2022-10-01 09:57:38 +0200

    Viz. class implementation is done.

--- visualize.py
+++ visualize.py
@@ -7,6 +7,7 @@
 # Distributed under terms of the MIT license.
 
 import os
+import subprocess
 import sys
 import random
 from pathlib import Path
@@ -54,9 +55,6 @@ class Viz:
             fp = str(Path(self.cfg.datapath, self.cfg.data_fnames[f]))
             self.data[f] = utils.read_pickle(fp)
 
-        # TODO: self.n_val_sids = list(val_samples.keys())
-        self.val_sids = ['00002', '00023', '00089', '00003']
-
         # Init. member vars
         self.l_samples = []
         self.n_samples = -1
@@ -77,13 +75,11 @@ class Viz:
                 raise TypeError('Either provide list of samples or # samples.')
             else:
                 # Random sample `n_samples` seqs. from total samples
+                raise NotImplementedError
 
-                # TODO: Remove next line once self.val_sids is implemented
                 n_samples = len(self.val_sids) \
                     if n_samples > len(self.val_sids) else n_samples
                 l_samples = random.sample(self.val_sids, n_samples)
-
-                # TODO: Support random train viz.
 
         # Viz. from l_samples
         else:
@@ -119,7 +115,22 @@ class Viz:
         return l_clids
 
 
-    def _create_seq2clid_df(self):
+    def _compress_l_clids(self, sid, idx, l_clids):
+        '''Convert list of clids -> (clid, length). Store in the format
+        described in _create_seq2clid_df_gt_cl(.).
+        '''
+        df_dict = {'name': [], 'idx': [], 'cluster': [], 'length': []}
+        for k, g in groupby(l_clids):
+            df_dict['name'].append(sid)
+            df_dict['idx'].append(idx)
+            idx += 1
+            df_dict['cluster'].append(k)
+            df_dict['length'].append(len(list(g)))
+        df = pd.DataFrame(data=df_dict)
+        return df, idx
+
+
+    def _create_seq2clid_df_gt_cl(self):
         '''
         DataFrame format:
         name    idx    cluster    length
@@ -127,7 +138,8 @@ class Viz:
 ...       ...    ...    ...         ...
 178     00034    16     890         8
         '''
-        df_ds = {'name': [], 'idx': [], 'cluster': [], 'length': []}
+        # Append each entry (idx) to this df
+        seq2clid_df = pd.DataFrame()
         idx = 1
 
         # Loop over each seq.
@@ -140,15 +152,40 @@ class Viz:
             # Get list of cluster ids for each frame in seq.
             l_clids = self._get_clids_for_seq(seq)
 
-            # Convert list of clids -> (clid, length)
-            for k, g in groupby(l_clids):
-                df_ds['name'].append(sid)
-                df_ds['idx'].append(idx)
-                idx += 1
-                df_ds['cluster'].append(k)
-                df_ds['length'].append(len(list(g)))
+            # Compress list of cl. IDs --> (cl. IDs, counts). See above.
+            df, idx = self._compress_l_clids(sid, idx, l_clids)
 
-        seq2clid_df = pd.DataFrame(data=df_ds)
+            # Append to common dataFrome
+            seq2clid_df = pd.concat([seq2clid_df, df])
+
+        return seq2clid_df
+
+
+    def _create_seq2clid_df_preds(self, l_seq_clids):
+        '''
+        Args:
+            l_seq_clids <list>: of <np.array> of cluster ID <int> for each seq.
+
+        Return:
+            seq2clid_df <pd.DataFrame>. Format:
+                name    idx    cluster    length
+        1       00009    1      953         13
+        ...       ...    ...    ...         ...
+        178     00034    16     890         8
+        '''
+        # Append each entry (idx) to this df
+        seq2clid_df = pd.DataFrame()
+        idx = 1
+
+        # Loop over each seq.
+        for sid, seq_clids in zip(self.l_samples, l_seq_clids):
+
+            # Compress list of cl. IDs --> (cl. IDs, counts). See above.
+            df, idx = self._compress_l_clids(sid, idx, seq_clids)
+
+            # Append to common dataFrome
+            seq2clid_df = pd.concat([seq2clid_df, df])
+
         return seq2clid_df
 
 
@@ -163,7 +200,7 @@ class Viz:
         for rec_type in self.cfg.rec_type:
             us_frac = float(self.cfg.fps.out_fps) / self.cfg.fps.pred_fps
             reconstruction(rec_type, self.cfg.filters, self.l_samples,
-                self.data['gt'], 'kitml', us_frac, 0.0, self.cfg.fps.out_fps,
+                self.data['gt'], 'kitml', us_frac, 1.0, self.cfg.fps.out_fps,
                 frames_dir, None,
                 False, contiguous_frame2cluster_mapping_path=seq2clid_df,
                 cluster2frame_mapping_path=self.data['clid2frame'])
@@ -171,31 +208,51 @@ class Viz:
 
     def sample_temos_asymov(self):
         '''
+        Eg., path for model predictions (npy files):
+        packages/TEMOS/outputs/kit-xyz-motion-word/asymov_full_run_1/uoon5wnl/samples/neutral_0ade04bd-954f-49bd-b25f-68f3d1ab8f1a
         '''
         ckpt_p = Path(self.cfg.path, self.cfg.approaches.asymov_temos)
 
-        # TODO: Below code doesn't run
-        sample_args = f'folder={ckpt_p.parent.parent} split={self.split_file_p.name} ckpt_name={ckpt_p.name}'
-        os.chdir('packages/TEMOS')
-        cmd = f'HYDRA_FULL_ERROR=1 python sample_asymov.py {sample_args}'
-        print(f'Run: ', cmd)
-        pdb.set_trace()
-        os.system(cmd)
-        
-        # Relies on cfg at configs/sample_asymov.yaml
+        cmd = ['python', 'sample_asymov.py']
+
+        # Overwrite cfg at configs/sample_asymov.yaml
+        cmd.append(f'folder={ckpt_p.parent.parent}')
+        cmd.append(f'split={self.split_file_p.name}')
+        cmd.append(f'ckpt_name={ckpt_p.name}')
+        print(f'Run: ', ' '.join(cmd))
+
+        # Forward pass, store predictions
+        subprocess.call(cmd, cwd=str(self.cfg.path))
+
+        # Covert clids --> frames
+        clid2kp = np.array(self.data['clid2kp']['keypoints3d'])
+
+        # Destination npy files
+        npy_folder = ckpt_p.parent.parent / 'samples' / f'neutral_{self.split_file_p.name}'
+
+        # Get predicted cluster IDs for all seqs. @ 12.5 fps
+        l_seq_clids = []
+        for sid in self.l_samples:
+            kp = np.array(np.load(f'{npy_folder}/{sid}.npy'), dtype=np.int64)
+            l_seq_clids.append(kp)
+
+        # Collate preds into specific compressed dataFrame
+        seq2clid_df = self._create_seq2clid_df_preds(l_seq_clids)
+
+        self.viz_diff_rec_types(seq2clid_df, 'asymov_temos')
 
 
     def sample_temos_bl(self):
         '''Sample and visualize seqs. specified in `self.l_samples`. Use TEMOS's sampling
         and viz. code out of the box.
 
-        For testing purposes: 
+        For testing purposes:
             <file_p>: packages/TEMOS/datasets/kit-splits/0d6f926b-52e9-4786-a1ae-1bd7dcf8d592
         '''
 
         # Load "latest ckpt" from folder spec. in viz.yaml. Overwrite TEMOS's sample cfg.
         folder = Path(self.cfg.path, self.cfg.approaches.temos_bl)
-        
+
         # TEMOS's sampling script saves pred xyz motions as npy's in ${folder}/samples/${split}
         sample_args = f'folder={folder} split={self.split_file_p.name}'
         os.chdir('packages/TEMOS')
@@ -203,7 +260,7 @@ class Viz:
         print(f'Run: ', cmd)
         os.system(cmd)
 
-        # Destination npy files: 
+        # Destination npy files:
         npy_folder = folder / 'samples' / f'neutral_{self.split_file_p.name}'
         keypoints = []
         for sid in self.l_samples:
@@ -212,8 +269,8 @@ class Viz:
             kp = viz.downsample(kp, 0.3)
             keypoints.append(kp)
 
-        # Visualize 
-        viz.viz_seq(self.l_samples, keypoints, Path(self.cfg.viz_dir, 'temos_bl'), 'kitml_temos', 25.0)
+        # Visualize
+        viz.viz_l_seqs(self.l_samples, keypoints, Path(self.cfg.viz_dir, 'temos_bl'), 'kitml_temos', 25.0)
 
 
     def viz_seqs(self, **kwargs):
@@ -224,6 +281,7 @@ class Viz:
             >>> viz_obj = Viz(cfg_p)
             >>> viz_obj.viz_seqs(n_samples=10)
             >>> viz_obj.viz_seqs(l_samples=['00002', '45', 2343, 9999])
+            >>> viz_obj.viz_seqs(l_samples=['00002', '45', 2343], n_samples=1)
             >>> viz_obj.viz_seqs(l_samples=['00002', '45', 2343], n_samples=1)
         '''
 
@@ -243,7 +301,7 @@ class Viz:
 
         # Reconstruct with GT Cluster ids
         if self.cfg.approaches.gt_clid:
-            seq2clid_df = self._create_seq2clid_df()  # GT cl ids for seqs.
+            seq2clid_df = self._create_seq2clid_df_gt_cl()  # GT cl ids for seqs.
             self.viz_diff_rec_types(seq2clid_df, 'gt_cluster_recon')
 
         # Create temp file in kit-splits that sample.py can load.
@@ -251,7 +309,7 @@ class Viz:
         self.split_file_p = Path('packages/TEMOS/datasets/kit-splits', str(uuid.uuid4()))
         utils.write_textf('\n'.join(self.l_samples), self.split_file_p)
         print('Created input seq. list file: ', self.split_file_p)
-        
+
         # Reconstruct with TEMOS-ASyMov model predictions
         if self.cfg.approaches.asymov_temos:
             self.sample_temos_asymov()  # Get pred cl ids foj
@@ -271,6 +329,11 @@ if __name__ == '__main__':
     # viz_obj.viz_seqs(n_samples=10)
     # viz_obj.viz_seqs(n_samples=2)
     # viz_obj.viz_seqs(l_samples=['00002', '45', 2343, 9999])
-    viz_obj.viz_seqs(l_samples=['00002', '45', 2343], n_samples=2)
+    # viz_obj.viz_seqs(l_samples=['00002', '45', 2343], n_samples=2)
+    # viz_obj.viz_seqs(l_samples=['00002', '45', 2343], n_samples=2)
 
+    # TODO: Enable directly giving split name in viz.yaml
+    filepath = Path(viz_obj.cfg.path, 'datasets/kit-splits-tiny/visu')
+    l_samples = utils.read_textf(filepath, ret_type='list')
+    viz_obj.viz_seqs(l_samples=l_samples)
 
