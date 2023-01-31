a2301d71b1c35048d5bed21566072e50db6324e1
darshkaushik <darsh.kaushik@gmail.com>
2023-01-31 20:27:12 +0530

    residual trajectory data included

--- packages/TEMOS/temos/data/tools/collate.py
+++ packages/TEMOS/temos/data/tools/collate.py
@@ -90,24 +90,37 @@ def mw_transform(mw_cluster_ids: List[int], special_symbols: List[str]):
                              mw_token_ids,
                              torch.tensor([special_symbols.index('<eos>')]))) # Add BOS/EOS assuming special first
     return mw_token_ids
-    
-def collate_motion_words_and_text_mt(lst_elements: List, text_vocab:Vocab, special_symbols: List[str]) -> Dict:
-    # pdb.set_trace()
+
+def traj_transform(traj_xyz: Tensor):
+    traj_xyz = traj_xyz.float() #double to float
+    traj_xyz = torch.cat((traj_xyz.new_zeros((1,3)),
+                          traj_xyz,
+                          traj_xyz.new_zeros((1,3))))
+    return traj_xyz
+
+def collate_motion_words_and_text_mt(lst_elements: List, text_vocab:Vocab, special_symbols: List[str], traj: bool = True) -> Dict:
 
     text_batch, mw_batch = [], []
     for x in lst_elements:
         text_sample, mw_sample = x['text'], x['motion_words']
         text_batch.append(text_transform(text_sample.rstrip("\n"), text_vocab))
         mw_batch.append(mw_transform(mw_sample, special_symbols))
-    
+
     PAD_IDX = text_vocab.__getitem__('<pad>')
     assert PAD_IDX == special_symbols.index('<pad>')
+    
     batch = {
         "text": pad_sequence(text_batch, padding_value=PAD_IDX), #[Frames, Batch size]
         "motion_words": pad_sequence(mw_batch, padding_value=PAD_IDX), #[Frames, Batch size]
         "text_length": [len(x) for x in text_batch],
-        "mw_length": [len(x) for x in mw_batch]
+        "length": [len(x) for x in mw_batch]
         }
+    
+    if traj:
+        # pdb.set_trace()
+        traj_batch = [traj_transform(x['traj']) for x in lst_elements] #List[Tensor[Frames, 3]]
+        # batch["traj"] = traj_batch
+        batch["traj"] = pad_sequence(traj_batch, padding_value=0.0) #[Frames, Batch size, 3]
     
     otherkeys = [x for x in lst_elements[0].keys() if x not in batch]
     for key in otherkeys:
